6b1ade7bcf8560a05c22dfe592ff6913b263d642
Razbibul <bimmer02@yandex.ru>
2023-01-29 16:37:48 +0500

    update code

--- Module 4/Lesson3_step1_15/Step_2/pages/basket_page.py
+++ Module 4/Lesson3_step1_15/Step_2/pages/basket_page.py
@@ -3,3 +3,8 @@ from .locators import LoginPageLocators
 
 
 class BasketPage(BasePage):
+    def should_not_be_products(self):
+        assert self.is_not_element_present(*BasketPageLocators.product_in_basket), "Product in basket, but shoul not be"
+
+    def should_be_message_about_empty_basket(self):
+        assert self.is_element_present(*BasketPageLocators.message_about_empty_basket),
--- Module 4/Lesson3_step1_15/Step_2/pages/locators.py
+++ Module 4/Lesson3_step1_15/Step_2/pages/locators.py
@@ -1,11 +1,30 @@
 from selenium.webdriver.common.by import By
 
 
-class LoginPageLocators():
-    BASKET_BUTTON = (By.XPATH, "//button[@class='btn btn-lg btn-primary btn-add-to-basket']")
-    NAME_STORE = (By.XPATH, "//div[@class='col-sm-6 product_main']/h1")
-    NAME_STORE_FINALLY = (By.XPATH, "//div[@class='alertinner']/strong")
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
-class BasePageLocators_2():
+class LoginPageLocators():
+    register_form = (By.CSS_SELECTOR, '#register_form')
+    login_form = (By.CSS_SELECTOR, "#login_form")
+    register_form_login = (By.CSS_SELECTOR, "#id_registration-email")
+    register_form_password1 = (By.CSS_SELECTOR, "#id_registration-password1")
+    register_form_password2 = (By.CSS_SELECTOR, "#id_registration-password2")
+    register_form_button = (By.CSS_SELECTOR, "[name='registration_submit']")
+
+class ProductPageLocators():
+    button_add_into_basket = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    find_product_name = (By.CSS_SELECTOR, ".product_main h1")
+    find_product_price = (By.CSS_SELECTOR, ".product_main p")
+    basket_find_product_name = (By.CSS_SELECTOR, "#messages .alert:nth-child(1) strong")
+    basket_find_product_price = (By.CSS_SELECTOR, "#messages .alert:nth-child(3) strong")
+
+class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    go_to_basket = (By.CSS_SELECTOR, ".btn-group .btn-default:nth-child(1)")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
+
+class BasketPageLocators():
+    product_in_basket = (By.CSS_SELECTOR, ".basket-items")
+    message_about_empty_basket = (By.CSS_SELECTOR, "#content_inner>p")
