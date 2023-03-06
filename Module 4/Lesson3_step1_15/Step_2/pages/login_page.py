8a6a3bcb3dcea85eab32bef69bd7235aab82e369
Razbibul <105239114+Razbibul@users.noreply.github.com>
2023-03-06 13:18:57 +0500

    Update

--- Module 4/Lesson3_step1_15/Step_2/pages/login_page.py
+++ Module 4/Lesson3_step1_15/Step_2/pages/login_page.py
@@ -20,10 +20,10 @@ class LoginPage(BasePage):
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        assert self.is_element_present(*LoginPageLocators.LOGIN_FORMA), "НЕТ ФОРМЫ АВТОРИЗАЦИИ ДАУН"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORMA), "НЕТ ФОРМЫ АВТОРИЗАЦИИ"
         #assert True
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert self.is_element_present(*LoginPageLocators.ATHORIZED_FORMA), "НЕТ ФОРМЫ РЕГИСТРАЦИИ ДАУН"
+        assert self.is_element_present(*LoginPageLocators.ATHORIZED_FORMA), "НЕТ ФОРМЫ РЕГИСТРАЦИИ"
         #assert True
